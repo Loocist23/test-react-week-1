7f6cbfe1b6a11a45d498c4c6af9bcabd22b46b09
Anthony <anthonyzegnal05@gmail.com>
2023-04-22 18:47:29 +0200

    bonjour

--- src/App.js
+++ src/App.js
@@ -7,19 +7,47 @@ function MyButton() {
         )
 }
 
-function App() {
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+      <li
+          key={product.id}
+          style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+          }}
+      >
+        {product.title}
+      </li>
+  );
+
+  return (
+      <ul>{listItems}</ul>
+  );
+}
+
+function About(){
+    return(
+        <div>
+            <>
+                <h1>About</h1>
+                <p>Hello there.<br />How do you do?</p>
+            </>
+        </div>
+    )
+}
+
+
+export default function App() {
   return (
       <div>
-          <>
-              <h1>About</h1>
-              <p>Hello there.<br />How do you do?</p>
-          </>
+            <About />
       </div>
 
   );
 }
-function About(){
 
-}
-
-export default App;
